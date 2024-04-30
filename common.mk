df40d57a0ea21b54d168d65c8d6f3fbc4e6e5df1
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-04-30 12:43:46 +0530

    spartan: Introduce AyakaAddons
    *Note: this is XiaomiParts imported to this tree. All credits goes to the authors of the following commits in below link
    src: https://github.com/Xiaomi-pad-6-development/android_device_xiaomi_sm8250-common/commits/derp-14/parts
    
    Signed-off-by: DragonEmperor9480 <amruteshnaregal1234@gmail.com>

--- common.mk
+++ common.mk
@@ -77,6 +77,14 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# Authsecret
+PRODUCT_PACKAGES += \
+    android.hardware.authsecret@1.0.vendor
+
+#AyakaAddons
+PRODUCT_PACKAGES += \
+    AyakaAddons
+
 # Bluetooth
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
